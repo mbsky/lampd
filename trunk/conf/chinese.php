7b02cca4c95d654f9a0785851bb398b56ae1c3d9
a950216t <a950216t@98a2e40d-cc6d-c76e-5375-3ca82545913e>
2015-01-22 10:14:10 +0000

    正式上傳

---
+++ trunk/conf/chinese.php
@@ -0,0 +1,71 @@
+<?php
+  // Traiditional Chinese translated by chinsan <chinsan.tw@gmail.com>
+
+  $CharSet = "UTF-8";
+
+  // 全局設置 (Global)
+  $Translate[0]  = "PureFTPd 帳號管理";
+  $Translate[1]  = "Version 2.1";
+
+  // 預設帳號 (Default username) 
+  $Translate[10] = "myftp";
+
+  // 系統信息 (System messages)
+  $Translate[20] = "所選擇的帳號或密碼不存在。";
+  $Translate[21] = "密碼不一致，當前的設置不會保存。";
+  $Translate[22] = "除了目前的密碼外，設定值都會被保存。";
+  $Translate[23] = "設定值將會被保存。";
+  $Translate[24] = "該ftp 帳號";
+  $Translate[25] = "將會被刪除。";
+  $Translate[26] = "錯誤: 無法存取任何目錄";
+
+  // 標籤說明 (Labels)
+  $Translate[30] = "登入頁";
+  $Translate[31] = "按此新增帳號";
+  $Translate[32] = "帳號";
+  $Translate[33] = "UID";
+  $Translate[34] = "GID";
+  $Translate[35] = "主目錄";
+  $Translate[36] = "上傳帶寬(KB/s)";
+  $Translate[37] = "下載帶寬(KB/s)";
+  $Translate[38] = "IP 地址";
+  $Translate[39] = "設置";
+  $Translate[40] = "啟用";
+  $Translate[41] = "密碼";
+  $Translate[42] = "再次確認密碼";
+  $Translate[43] = "文件配額";
+  $Translate[44] = "磁盤配額(MB)";
+  $Translate[45] = "上傳／下載比 [上:下]";
+  $Translate[46] = "備註";
+
+  // 按鈕信息 (Button info)
+  $Translate[60] = "修改";
+  $Translate[61] = "刪除";
+  $Translate[62] = "鎖定";
+  $Translate[63] = "解鎖";
+  $Translate[64] = "開啟";
+  $Translate[65] = "選擇目錄";
+  $Translate[66] = "關閉瀏覽目錄";
+  $Translate[67] = "保存";
+
+  // 在瀏覽目錄時的各項屬性說明 (Attributes from the directory browser)
+  $Translate[80] = "名稱";
+  $Translate[81] = "大小";
+  $Translate[82] = "類型";
+  $Translate[83] = "修改日期";
+  $Translate[84] = "擁有者";
+  $Translate[85] = "群組";
+  $Translate[86] = "屬性";
+
+  // 輔助說明 (Help balloons) 
+  $Translate[90] = "帳號只有『啟用』被選取時，才會生效。";
+  $Translate[91] = "該 ftp 帳號所要使用的 UID (帳號權限)";
+  $Translate[92] = "該 ftp 帳號所要使用的 GID (群組權限)";
+  $Translate[93] = "上傳文件時的帶寬限制(單位: KB/s)";
+  $Translate[94] = "下載文件時的帶寬限制(單位: KB/s)";
+  $Translate[95] = "限制此帳號所能存放的(最多的)文件數量。";
+  $Translate[96] = "限制此帳號所能存放的(最多的)空間大小。(單位: Mb)";
+  $Translate[97] = "比如 <1:2> 代表的是: 至少先上傳4Mb 才能下載 8Mb，而 <0:0> 則是關閉上下傳比限制";
+  $Translate[98] = "限制此帳號只能由特定 IP 來源，或是任何地方均可使用 (*)";
+
+?>
